e694b98d6485f737f40c48f07a193cff43c2b74b
Gabriel1Ribeiro <gabriel.hribeiro@sptech.school>
2025-11-06 23:07:52 -0300

    Enviando a atividade

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,92 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vaga;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public Integer getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Integer vaga) {
+        this.vaga = vaga;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void contratar(Desenvolvedor desenvolvedor) {
+        if (getVaga() > desenvolvedores.size()) {
+            desenvolvedores.add(desenvolvedor);
+        }else{
+            System.out.println("Não há vagas o suficiente.");
+        }
+    }
+
+    public void contratarFullstack(DesenvolvedorWeb desenvolvedor) {
+        if (desenvolvedor.isFullstack() == true) {
+            desenvolvedores.add(desenvolvedor);
+        }else{
+            System.out.println("O desenvolvedor não é fullstack.");
+        }
+    }
+
+    public Double getTotalSalarios() {
+        double total = 0.0;
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            total += dev.calcularSalario();
+        }
+        return total;
+    }
+
+    public Integer qtdDesenvolvedoresMobile() {
+        int total = 0;
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev instanceof DesenvolvedorMobile) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario) {
+        List<Desenvolvedor> resultado = new ArrayList<>();
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev.calcularSalario() >= salario) {
+                resultado.add(dev);
+            }
+        }
+
+        return resultado;
+    }
+
+    public Desenvolvedor buscarMenorSalario() {
+        if (desenvolvedores.isEmpty()) {
+            return null;
+        }
+        Desenvolvedor menor = desenvolvedores.get(0);
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev.calcularSalario() < menor.calcularSalario()) {
+                menor = dev;
+            }
+        }
+
+        return menor;
+    }
+
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
@@ -1,4 +1,49 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorMobile {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorMobile extends Desenvolvedor {
+    private String plataforma;
+    private String linguagem;
+    private Integer horasPrototipacao;
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+
+    public Integer getHorasPrototipacao() {
+        return horasPrototipacao;
+    }
+
+    public void setHorasPrototipacao(Integer horasPrototipacao) {
+        this.horasPrototipacao = horasPrototipacao;
+    }
+
+    public DesenvolvedorMobile() {
+    }
+
+    public DesenvolvedorMobile(String nome, Integer qtdHoras, Double valorHora, String plataforma, String linguagem, Integer horasPrototipacao) {
+        super(nome, qtdHoras, valorHora);
+        this.plataforma = plataforma;
+        this.linguagem = linguagem;
+        this.horasPrototipacao = horasPrototipacao;
+    }
+
+    @Override
+    public Double calcularSalario() {
+        return (qtdHoras * valorHora) + (horasPrototipacao * 200);
+    }
+
 }
